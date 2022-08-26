0369c8af2a92f71a75fc6b4628bac700d07f168a
Paulo Sergio <pauloserg861@gmail.com>
2022-08-26 16:45:42 -0300

    adicionando projeto com JSX

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script lang="jsx">
+import { defineComponent } from 'vue'
+import Todo from './components/Todo.vue'
+
+import './assets/base.css'
+
+const App = defineComponent({
+  name: 'App',
+  render() {
+    return (
+      <div>
+        <Todo />
+      </div>
+    )
+  }
+})
+
+export default App
+</script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,63 @@
+<script lang="jsx">
+import { defineComponent, reactive } from 'vue'
+
+import styles from '../assets/Main.module.css'
+
+const Todo = defineComponent({
+  name: 'Todo',
+  setup() {
+    const state  = reactive({
+      items: [
+        /*{ id: Math.random().toPrecision(10), task: 'studing Vue JSX' },
+        { id: Math.random().toPrecision(10), task: 'studing Vue Setup' }*/
+      ],
+      newItem: ''
+    })
+
+    function handleSetNewItem(e) {
+      state.newItem = e.target.value
+    }
+
+    function handleAddToList() {
+      if (state.newItem !== '') {
+        state.items.push({
+          id: Math.random().toPrecision(10),
+          task: state.newItem
+        })
+        state.newItem = ''
+      }  
+    }
+
+    return {
+      state,
+      handleSetNewItem,
+      handleAddToList,
+    }
+  },
+  render () {
+    return (
+      <div className={styles.container}> 
+        <div className={styles.box_input}>
+          <input 
+            type="text" 
+            placeholder="Digite uma tarefa" 
+            onChange={this.handleSetNewItem}
+          />
+          <button onClick={this.handleAddToList}>Enter a Task</button>
+        </div>
+        <div className={styles.box_tasks}>
+          <ul>
+            {
+              this.state.items.map((item, index) => (
+                <li key={item.id} className={styles.list_item}>{item.task}</li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
+    )
+  }
+})
+
+export default Todo
+</script>
